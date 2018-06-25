b51d7138844a786a3fd1b98380c8c10df58b2f7d
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-06-25 13:21:49 -0500

    created api/data.js

---
+++ src/api/data.js
@@ -0,0 +1,60 @@
+const students = [
+  {
+    id: 1,
+    name: 'Rafael',
+    games: ['Football', 'Cricket', 'Tennis'],
+    selected: false,
+
+  },
+  {
+    id: 2,
+    name: 'Lisa',
+    games: ['Balleyball', 'Football'],
+    selected: false,
+  },
+  {
+    id: 3,
+    name: 'Mery',
+    games: ['Video Games', 'Swimming'],
+    selected: false,
+  },
+  {
+    id: 4,
+    name: 'Mario',
+    games: ['Running', 'Q&A'],
+    selected: false,
+  },
+  {
+    id: 5,
+    name: 'Brian',
+    games: ['Tennis', 'Balleyball', 'Pool'],
+    selected: false,
+  },
+  {
+    id: 6,
+    name: 'Enrico',
+    games: ['Balleyball', 'Football'],
+    selected: false,
+
+  },
+  {
+    id: 7,
+    name: 'Jesus',
+    games: ['Balleyball', 'Football'],
+    selected: false,
+
+  },
+  {
+    id: 8,
+    name: 'Carla',
+    games: ['Balleyball', 'Football'],
+    selected: false,
+
+  },
+];
+
+export default {
+  getStudents() {
+    return students;
+  },
+};
